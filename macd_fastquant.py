2f6fb339fabb87d2ea7424dca28b43e9a5ea8700
sunnycqcn <fufuyou@gmail.com>
2021-04-03 11:58:54 -0600

    Add files via upload

---
+++ macd_fastquant.py
@@ -0,0 +1,6 @@
+
+from fastquant import backtest, get_stock_data
+df = get_stock_data("AAAA", "2018-01-01", "2021-04-01")
+
+res,plot = backtest('macd', df, init_cash=2000, fast_period=12, slow_period=26, signal_period=9, sma_period=30, dir_period=10, return_plot=True)
+#plot.savefig('AAAA_macd.png')
